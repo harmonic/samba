056a37d69db1296326c76178eacf579dfb83b83c
Devin Schmitz <devin@temporal.xyz>
2026-08-20 19:25:45 +0000

    Harmonic: Add strategy config option, memo, and set-strategy command

--- src/discof/replay/fd_replay_tile.c
+++ src/discof/replay/fd_replay_tile.c
@@ -4594,7 +4594,7 @@ unprivileged_init( fd_topo_t const *      topo,
              (fd_acct_addr_t const *)tile->replay.bundle.tip_distribution_program_addr,
              (fd_acct_addr_t const *)tile->replay.bundle.tip_payment_program_addr,
              (fd_acct_addr_t const *)ctx->bundle.vote_account.uc,
-             (fd_acct_addr_t const *)ctx->bundle.vote_account.uc, "NAN", 0UL ) ) ) {
+             (fd_acct_addr_t const *)ctx->bundle.vote_account.uc, 0UL ) ) ) {
       FD_LOG_ERR(( "failed to initialize bundle crank gen" ));
     }
   }
